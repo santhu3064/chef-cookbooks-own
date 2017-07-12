5704721964c36955d6146d1c041c5a79587a464c
Aamir Ahmad <santhu3064@gmail.com>
2017-07-12 07:43:54 +0530

    new

--- .chef/local-mode-cache/cache/cookbooks/apache/recipes/default.rb
+++ .chef/local-mode-cache/cache/cookbooks/apache/recipes/default.rb
@@ -1,6 +1,6 @@
 #
-# Cookbook:: workstation
+# Cookbook:: apache
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe 'workstation::setup'
+include_recipe "apache::ser"
